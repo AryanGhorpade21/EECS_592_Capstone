0a217548e24ba4db422940d71f93845d41c4382a
AryanGhorpade21 <143010639+AryanGhorpade21@users.noreply.github.com>
2026-03-01 19:57:29 -0600

    added how to run it on AWS

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -10,6 +10,24 @@ for AWS Lambda functions. For the AWS environment, the Lambda function will be t
 (e.g., a scheduled event or an API call),and it will execute the check_rules function to check for 
 any broken rules and send notifications accordingly.
 """
+
+"""
+This Is how you can deploy it in AWS server
+
+Zip everything
+
+zip -r alert_lambda.zip alert_gentrator.py lambda_function.py src/
+
+Create Lambda function
+
+aws lambda create-function
+--function-name NetworkAlertChecker
+--runtime python3.11
+--role arn:aws:iam::YOUR_ACCOUNT:role/lambda-sns-role
+--handler lambda_function.lambda_handler
+--zip-file fileb://alert_lambda.zip
+"""
+
 from alert_gentrator import check_rules
 
 def lambda_handler(event, context):
